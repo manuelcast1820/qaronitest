e4f24ce63a1383dd3a78c39dea19c3836733039c
Manuel <mcastellanotg@gmail.com>
2021-12-29 22:01:55 -0400

    begin repo

---
+++ database/migrations/2021_12_30_012009_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoryId')
+                ->constrained('categories');
+            $table->dateTime('date')->nullable();
+            $table->string('slug')->unique();
+            $table->integer('capacity');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
